b2f13b7a25d83ec055da03eefa65ca6ee10cf694
Ankit Kumar <akumar-ext@skillz.com>
2026-02-23 09:46:05 +0530

    fix error

--- src/components/PhoneLinks/PhoneLinks.tsx
+++ src/components/PhoneLinks/PhoneLinks.tsx
@@ -1,36 +1,56 @@
 import React from "react";
 import styles from "./PhoneLinks.module.scss";
 import Image from "next/image";
+import { generateAFOneLink } from "@/utils/generateOneLink";
 
 interface PhoneLinksProps {
   applePlayUrl: string;
   androidPlayUrl: string;
+  width?: number;
+  height?: number;
 }
 
 const PhoneLinks: React.FC<PhoneLinksProps> = ({
   applePlayUrl,
   androidPlayUrl,
+  width = 180,
+  height = 60,
 }) => {
+  const openLink = async (url: string) => {
+    const appsFlyerResult = await generateAFOneLink(url);
+    const clickURL = appsFlyerResult?.clickURL;
+    if (!clickURL) {
+      window.location.href = url;
+      return;
+    }
+    setTimeout(() => {
+      if (window.AF_SMART_SCRIPT?.fireImpressionsLink) {
+        window.AF_SMART_SCRIPT.fireImpressionsLink();
+      }
+      window.location.href = clickURL;
+    }, 500);
+  };
+
   return (
     <div className={styles.imageContainer}>
-      <a href={applePlayUrl} target="_blank" rel="noopener noreferrer">
+      <div onClick={() => openLink(applePlayUrl)}>
         <Image
           src="/images/AppleStore.webp"
           alt="apple store"
           className={styles.StoreImage}
-          width={180}
-          height={60}
+          width={width}
+          height={height}
         />
-      </a>
-      <a href={androidPlayUrl} target="_blank" rel="noopener noreferrer">
+      </div>
+      <div onClick={() => openLink(androidPlayUrl)}>
         <Image
           src="/images/android_apk.webp"
           alt="android store"
           className={styles.androidStoreImage}
-          width={180}
-          height={60}
+          width={width}
+          height={height}
         />
-      </a>
+      </div>
     </div>
   );
 };
